6125119e224a004052fef14a6de461b616813160
Pedro Oliveira <pedro.freire.oliveira@tecnico.ulisboa.pt>
2026-05-12 02:27:32 +0100

    Finished the REPL!

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,30 @@
 module Main (main) where
 
+import System.IO (hFlush, stdout)
+import Parser
+import Types
+
+read' :: IO String
+read' = putStr "λ> " >> hFlush stdout >> getLine
+
+eval' :: String -> Maybe Expression
+eval' s = case parse expression s of
+  Just (e, _) -> Just e
+  Nothing -> Nothing
+
+print' :: Maybe Expression -> String
+print' e = case e of
+  Just e' -> show e'
+  Nothing -> "Whoops, Parsing failed!"
+
+loop' :: IO ()
+loop' = do
+  input <- read'
+  case input of
+    ":q" -> return ()
+    _    -> do
+            putStrLn (print' (eval' input))
+            loop' 
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = loop'
